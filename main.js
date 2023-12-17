9c4f8d01e448926e31d15caa89cb4216e79446a2
Deb Pramanik <36265065+debpramanik@users.noreply.github.com>
2023-12-17 22:59:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
         var now = new Date().getTime();
         if (now >= releaseTime) {
             container.innerHTML = `
-            <a href="https://drive.google.com/uc?export=download&id=1dH-17nYXq9xyw2kcX3cCiA5Rm7JZtd74" download>
-            <img src="assets/souvenir.jpeg" alt="IDA Silver Jubilee Ebook Cover" class="ebook-cover">
-            <h2 class="ebook-title">SOUVENIR</h2>
-        </a>
+                <a href="https://drive.google.com/uc?export=download&id=1dH-17nYXq9xyw2kcX3cCiA5Rm7JZtd74" download>
+                    <img src="assets/souvenir.jpeg" alt="IDA Silver Jubilee Ebook Cover" class="ebook-cover">
+                    <h2 class="ebook-title">SOUVENIR</h2>
+                </a>
             `; // Add the HTML content for the ebook
             clearInterval(checkTime);
         }
