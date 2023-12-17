4c489b03f1df9cdffe7c21cea4202bd83a78e823
Deb Pramanik <36265065+debpramanik@users.noreply.github.com>
2023-12-17 09:32:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (now >= releaseTime) {
             container.innerHTML = `
             <a href="https://drive.google.com/uc?export=download&id=1dH-17nYXq9xyw2kcX3cCiA5Rm7JZtd74" download>
-            <img src="assets/souvenier.png" alt="IDA Silver Jubilee Ebook Cover" class="ebook-cover">
+            <img src="assets/souvenir.jpeg" alt="IDA Silver Jubilee Ebook Cover" class="ebook-cover">
             <h2 class="ebook-title">Souvenier</h2>
         </a>
             `; // Add the HTML content for the ebook
